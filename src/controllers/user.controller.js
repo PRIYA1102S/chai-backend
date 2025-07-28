56be253d55491b576c06fe4e09ff99c68476b037
priya <ps7701124@gmail.com>
2025-07-28 22:24:09 +0530

    controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/users.models.js";
+import { uploadOnCloudinary } from "../utils/Cloudinary.js";
+
+
+
 
 const registerUser = asyncHandler(async(req,res) => {
    const {fullName, email, username, password} = req.body
@@ -20,6 +24,22 @@ const registerUser = asyncHandler(async(req,res) => {
    if(existedUser)
    {
     throw new ApiError(409,"username or email with this are already exists")
+   }
+
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   if(!avatarLocalPath)
+   {
+       throw new ApiError(400,"Avatar file is required.")
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath)
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   if(!avatar)
+   {
+       throw new ApiError(400,"Avatar file is required.")
    }
 
 })
